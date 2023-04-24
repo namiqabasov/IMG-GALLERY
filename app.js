56e8658f8626bbecddf69145c2c70b80421c9203
namiqabasov <7td508c@code.edu.az>
2023-04-24 21:46:01 +0400

    IMG GALLERY

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const exportImg = document.getElementById("exportImg");
-
-const divimg = document.getElementsByClassName("divimg");
-
+const imgtext = document.getElementById("imgtext");
 const imgdog = document.getElementById("imgdog");
 const imgmain = document.getElementById("imgmain");
 const imginline2 = document.getElementById("imgshow");
 
 imgdog.onclick = function () {
   exportImg.src = imgdog.src;
-  exportImg.style.cssText=`
+  exportImg.style.cssText = `
   height: 200px;
-  width: 300px;`
+  width: 300px;`;
+  imgtext.innerHTML = imgdog.alt;
 };
 imgmain.onclick = function () {
   exportImg.src = imgmain.src;
-  exportImg.style.cssText=`
+  exportImg.style.cssText = `
   height: 200px;
-  width: 300px;`
+  width: 300px;`;
+  imgtext.innerHTML = imgmain.alt;
 };
 imgshow.onclick = function () {
   exportImg.src = imgshow.src;
-  exportImg.style.cssText=`
+  exportImg.style.cssText = `
   height: 200px;
-  width: 300px;`
+  width: 300px;`;
+  imgtext.innerHTML = imgshow.alt;
 };
