741e6307c37baa0c6672270e80c4beb7dbec4c99
namiqabasov <7td508c@code.edu.az>
2023-04-28 22:16:39 +0400

    IMG-GALLERY

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-const exportImg = document.getElementById("exportImg");
-const imgtext = document.getElementById("imgtext");
-const imgdog = document.getElementById("imgdog");
-const imgmain = document.getElementById("imgmain");
-const imginline2 = document.getElementById("imgshow");
+// const exportImg = document.getElementById("exportImg");
+// const imgtext = document.getElementById("imgtext");
+// const imgdog = document.getElementById("imgdog");
+// const imgmain = document.getElementById("imgmain");
+// const imginline2 = document.getElementById("imgshow");
 
-imgdog.onclick = function () {
-  exportImg.src = imgdog.src;
-  exportImg.style.cssText = `
-  height: 200px;
-  width: 300px;`;
-  imgtext.innerHTML = imgdog.alt;
-};
-imgmain.onclick = function () {
-  exportImg.src = imgmain.src;
-  exportImg.style.cssText = `
-  height: 200px;
-  width: 300px;`;
-  imgtext.innerHTML = imgmain.alt;
-};
-imgshow.onclick = function () {
-  exportImg.src = imgshow.src;
-  exportImg.style.cssText = `
-  height: 200px;
-  width: 300px;`;
-  imgtext.innerHTML = imgshow.alt;
-};
+// imgdog.onclick = function () {
+//   exportImg.src = imgdog.src;
+//   exportImg.style.cssText = `
+//   height: 200px;
+//   width: 300px;`;
+//   imgtext.innerHTML = imgdog.alt;
+// };
+// imgmain.onclick = function () {
+//   exportImg.src = imgmain.src;
+//   exportImg.style.cssText = `
+//   height: 200px;
+//   width: 300px;`;
+//   imgtext.innerHTML = imgmain.alt;
+// };
+// imgshow.onclick = function () {
+//   exportImg.src = imgshow.src;
+//   exportImg.style.cssText = `
+//   height: 200px;
+//   width: 300px;`;
+//   imgtext.innerHTML = imgshow.alt;
+// };
+
+const images = document.querySelectorAll(".container img");
+const selected = document.querySelector(".colum img");
+const text = document.querySelector(".colum div");
+
+console.log(images);
+
+selected.src = images[0].src;
+text.innerText = images[0].alt;
+
+images.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    selected.src = event.target.src;
+    text.innerText = event.target.alt;
+  });
+});
+
